af252ebe66d8c12946f96268f139b1ed63918f08
Alexander <al.zatulyviter@gmail.com>
2022-01-09 15:03:43 +0200

    try imgx

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   assetPrefix: isProd ? "/sonsliv.live/" : "",
   reactStrictMode: true,
+  images: {
+    loader: "imgix",
+    path: "/",
+  },
 };
