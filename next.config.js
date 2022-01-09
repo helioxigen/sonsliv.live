18d4b724c7c6a12c0887cecc775fa6ecde244876
Alexander <al.zatulyviter@gmail.com>
2022-01-09 15:31:07 +0200

    try no prefixz

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/sonsliv.live/" : "",
+  // assetPrefix: isProd ? "/sonsliv.live/" : "",
   reactStrictMode: true,
   images: {
     loader: "imgix",
