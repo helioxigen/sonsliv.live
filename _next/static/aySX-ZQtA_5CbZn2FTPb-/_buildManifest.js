f652dd76d7736a0329d9efe1df0f1335d71dddca
helioxigen <helioxigen@users.noreply.github.com>
2022-01-14 13:08:03 +0000

    deploy: 324f2f10feda51df56286c7229f704c65521819c

---
+++ _next/static/aySX-ZQtA_5CbZn2FTPb-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/167-76b3af4b614bf549.js","static/css/dedb7fd8f6e9151d.css","static/chunks/pages/index-3f66d1ce2b9276fa.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
